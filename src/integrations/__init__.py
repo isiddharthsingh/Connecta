9a1409c100d3bb4fd21e8b4eb3538abdbfde6c20
Siddharth Singh <singhsiddharth0127@gmail.com>
2025-06-19 00:32:47 -0400

    Update integrations module structure

--- src/integrations/__init__.py
+++ src/integrations/__init__.py
@@ -3,10 +3,12 @@
 from .base import BaseIntegration, APIError
 from .gmail import GmailIntegration
 from .github import GitHubIntegration
+from .calendar import CalendarIntegration
 
 __all__ = [
     'BaseIntegration',
     'APIError', 
     'GmailIntegration',
-    'GitHubIntegration'
+    'GitHubIntegration',
+    'CalendarIntegration'
 ] 
